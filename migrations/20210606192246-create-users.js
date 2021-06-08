d16e52e54d2605efe9ff23222da0942acde9bf8c
Ayesha <ayeshawtahir@gmail.com>
2021-06-08 17:30:42 +0500

    migartion file updated

--- migrations/20210606192246-create-users.js
+++ migrations/20210606192246-create-users.js
@@ -1,23 +1,37 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface, DataTypes) => {
     await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
+        type: DataTypes.INTEGER
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull:false
+      },
+      uuid: {
+        type: DataTypes.UUID,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        defaultValue: DataTypes.UUIDV4,
+        allowNull:false,
       },
       email: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: DataTypes.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: DataTypes.DATE
       }
     });
   },
