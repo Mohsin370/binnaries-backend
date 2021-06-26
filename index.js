4166ddf986cde35325adccccf2f54ca65da43747
mohsin <mohsinijaz13@gmail.com>
2021-06-27 00:56:04 +0500

    .

--- index.js
+++ index.js
@@ -11,21 +11,7 @@ require("dotenv").config();
 
 app.use(cors())
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Origin,Access-Control-Allow-Methods,Access-Control-Allow-Headers,Content-Type,Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-  next();
-});
+
 
 //test call
 app.get("/",(req,res)=>res.send("Server Running"))
